5b0c41f8df2b11f5ca3d396b74590c9a62f182c3
Stefan Sabev <stevenbg91@gmail.com>
2024-09-22 08:23:08 +0200

    Add routing in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import About from "./components/about/About"
+import AboutInstructor from "./components/aboutInstructor/AboutInstructor"
+import Banner from "./components/banner/Banner"
+import Brand from "./components/brand/Brand"
+import Courses from "./components/courses/Courses"
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import News from "./components/news/News"
+import Popular from "./components/popular/Popular"
+import Pricing from "./components/pricing/Pricing"
+import RegistrationSection from "./components/registrationSection/RegistrationSection"
+import ThemeController from "./components/themeController/ThemeController"
+import Login from "./components/auth/login/Login"
+import CoursesPage from "./components/courses/coursesPage/CoursesPage"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeController />
+      <Header />
+      <main className="bg-transparent">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Banner />
+              <Brand />
+              <About />
+              <Popular />
+              <Courses />
+              <RegistrationSection />
+              <Pricing />
+              <AboutInstructor />
+              <News />
+            </>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/courses" element={<CoursesPage />} />
+
+        </Routes>
+      </main>
+      <Footer />
     </>
   )
 }
