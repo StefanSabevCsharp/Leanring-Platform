ab25fb026c32bbe812203fe5ffe7d325629e7329
Stefan Sabev <stevenbg91@gmail.com>
2024-09-22 13:02:22 +0200

    Separate responsibilities for the forms.

--- client/src/components/auth/login/Login.jsx
+++ client/src/components/auth/login/Login.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useForm from "../../../hooks/useForm";
+import { toast, Toaster } from "react-hot-toast";
+
+import RegisterForm from "./registerForm/RegisterForm";
+import LoginForm from "./loginForm/loginForm";
+
 
 export default function Login() {
-
     const [isLogin, setIsLogin] = useState(true);
+    //TODO: Add error notification with toast or directly on the form
+
+    // useEffect(() => {
+    //     if (Object.keys(errors).length > 0) {
+    //         Object.values(errors).forEach(error => {
+    //             toast.error(error);
+    //         })
+    //     }
+    // }, [errors])
+
+
     return (
 
         <>
+            {/* <Toaster /> */}
             <section>
                 {/* banner section */}
                 <div className="bg-lightGrey10 dark:bg-lightGrey10-dark relative z-0 overflow-y-visible py-50px md:py-20 lg:py-100px 2xl:pb-150px 2xl:pt-40.5">
@@ -83,196 +100,8 @@ export default function Login() {
                             <div className="tab-contents">
                                 {/* login form*/}
                                 {isLogin ? (
-                                    <div className="block opacity-100 transition-opacity duration-150 ease-linear">
-                                        {/* heading   */}
-                                        <div className="text-center">
-                                            <h3 className="text-size-32 font-bold text-blackColor dark:text-blackColor-dark mb-2 leading-normal">
-                                                Login
-                                            </h3>
-                                            <p className="text-contentColor dark:text-contentColor-dark mb-15px">
-                                                Don't have an account yet?
-                                                <Link
-                                                    to="/login"
-                                                    onClick={() => setIsLogin(false)}
-                                                    className="ml-5 hover:text-primaryColor relative after:absolute after:left-0 after:bottom-0.5 after:w-0 after:h-0.5 after:bg-primaryColor after:transition-all after:duration-300 hover:after:w-full"
-                                                >
-                                                    Sign up for free
-                                                </Link>
-                                            </p>
-                                        </div>
-                                        <form className="pt-25px" data-aos="fade-up">
-                                            <div className="mb-25px">
-                                                <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                    Username or email
-                                                </label>
-                                                <input
-                                                    type="text"
-                                                    placeholder="Your username or email"
-                                                    className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                />
-                                            </div>
-                                            <div className="mb-25px">
-                                                <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                    Password
-                                                </label>
-                                                <input
-                                                    type="password"
-                                                    placeholder="Password"
-                                                    className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                />
-                                            </div>
-                                            <div className="text-contentColor dark:text-contentColor-dark flex items-center justify-between">
-                                                <div className="flex items-center">
-                                                    <input
-                                                        type="checkbox"
-                                                        id="remember"
-                                                        className="w-18px h-18px mr-2 block box-content"
-                                                    />
-                                                    <label htmlFor="remember"> Remember me</label>
-                                                </div>
-                                                <div>
-                                                    <a
-                                                        href="#"
-                                                        className="hover:text-primaryColor relative after:absolute after:left-0 after:bottom-0.5 after:w-0 after:h-0.5 after:bg-primaryColor after:transition-all after:duration-300 hover:after:w-full"
-                                                    >
-                                                        Forgot your password?
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <div className="my-25px text-center">
-                                                <button
-                                                    type="submit"
-                                                    className="text-size-15 text-whiteColor bg-primaryColor px-25px py-10px w-full border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark"
-                                                >
-                                                    Log in
-                                                </button>
-                                            </div>
-                                            {/* other login */}
-                                            {/* <div>
-                                                <p className="text-contentColor dark:text-contentColor-dark text-center relative mb-15px before:w-2/5 before:h-1px before:bg-borderColor4 dark:before:bg-borderColor2-dark before:absolute before:left-0 before:top-4 after:w-2/5 after:h-1px after:bg-borderColor4 dark:after:bg-borderColor2-dark after:absolute after:right-0 after:top-4">
-                                                    or Log-in with
-                                                </p>
-                                            </div>
-                                            <div className="text-center flex gap-x-1 md:gap-x-15px lg:gap-x-25px gap-y-5 items-center justify-center flex-wrap">
-                                                <button
-                                                    type="submit"
-                                                    className="text-size-15 text-whiteColor bg-primaryColor px-11 py-10px border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark"
-                                                >
-                                                    <i className="icofont-facebook" /> Facebook
-                                                </button>
-                                                <button
-                                                    type="submit"
-                                                    className="text-size-15 text-whiteColor bg-primaryColor px-11 py-10px border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark"
-                                                >
-                                                    <i className="icofont-google-plus" /> Google
-                                                </button>
-                                            </div> */}
-                                        </form>
-                                    </div>) : (
-                                    <div className="block opacity-100 transition-opacity duration-150 ease-linear">
-                                        {/* heading   */}
-                                        <div className="text-center">
-                                            <h3 className="text-size-32 font-bold text-blackColor dark:text-blackColor-dark mb-2 leading-normal">
-                                                Register
-                                            </h3>
-                                            <p className=" text-contentColor dark:text-contentColor-dark mb-15px">
-                                                Already have an account?
-                                                <Link
-                                                    to="/login"
-                                                    onClick={() => setIsLogin(true)}
-                                                    className="ml-5 hover:text-primaryColor relative after:absolute after:left-0 after:bottom-0.5 after:w-0 after:h-0.5 after:bg-primaryColor after:transition-all after:duration-300 hover:after:w-full"
-                                                >
-                                                    Log In
-                                                </Link>
-                                            </p>
-                                        </div>
-                                        <form className="pt-25px" data-aos="fade-up">
-                                            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        First Name
-                                                    </label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="First Name"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        Last Name
-                                                    </label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Last Name"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        Username
-                                                    </label>
-                                                    <input
-                                                        type="text"
-                                                        placeholder="Username"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        Email
-                                                    </label>
-                                                    <input
-                                                        type="email"
-                                                        placeholder="Your Email"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-30px gap-y-25px mb-25px">
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        Password
-                                                    </label>
-                                                    <input
-                                                        type="password"
-                                                        placeholder="Password"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <label className="text-contentColor dark:text-contentColor-dark mb-10px block">
-                                                        Re-Enter Password
-                                                    </label>
-                                                    <input
-                                                        type="password"
-                                                        placeholder="Re-Enter Password"
-                                                        className="w-full h-52px leading-52px pl-5 bg-transparent text-sm focus:outline-none text-contentColor dark:text-contentColor-dark border border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 font-medium rounded"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="text-contentColor dark:text-contentColor-dark flex items-center">
-                                                <input
-                                                    type="checkbox"
-                                                    id="accept-pp"
-                                                    className="w-18px h-18px mr-2 block box-content"
-                                                />
-                                                <label htmlFor="accept-pp">
-                                                    Accept the Terms and Privacy Policy
-                                                </label>
-                                            </div>
-                                            <div className="mt-25px text-center">
-                                                <button
-                                                    type="submit"
-                                                    className="text-size-15 text-whiteColor bg-primaryColor px-25px py-10px w-full border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark"
-                                                >
-                                                    Register
-                                                </button>
-                                            </div>
-                                        </form>
-                                    </div>)}
+                                    <LoginForm setIsLogin={setIsLogin} />) : (
+                                    <RegisterForm setIsLogin={setIsLogin} />)}
 
                                 {/* sign up form*/}
 
