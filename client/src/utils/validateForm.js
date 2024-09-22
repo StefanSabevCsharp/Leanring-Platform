20de65ef1c3f71763f8ee646cf1a24e27f0cb642
Stefan Sabev <stevenbg91@gmail.com>
2024-09-22 10:18:29 +0200

    Create basic frond-end validator for register form

---
+++ client/src/utils/validateForm.js
@@ -0,0 +1,33 @@
+import { isLength } from "validator";
+import isEmail from "validator/lib/isEmail";
+
+
+const validateRegisterForm = (values) => {
+    let errors = {};
+    const {firstName, lastName, email, username, password, rePassword} = values;
+
+    if (!isLength(firstName, { min: 3, max: 30 })) {
+        errors.firstName = "First name must be between 3 and 30 characters";
+    }
+    if (!isLength(lastName, { min: 3, max: 30 })) {
+        errors.lastName = "Last name must be between 3 and 30 characters";
+    }
+    if (!isLength(username, { min: 3, max: 30 })) {
+        errors.username = "Username must be between 3 and 30 characters";
+    }
+    if (!isEmail(email)) {
+        errors.email = "Invalid email";
+    }
+    if (!isLength(password, { min: 6, max: 30 })) {
+        errors.password = "Password must be between 6 and 30 characters";
+    }
+    if (password !== rePassword) {
+        errors.rePassword = "Passwords do not match";
+    }
+
+    return errors;
+
+}
+export {
+    validateRegisterForm,
+}
