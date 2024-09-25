f67019343a639c2e14da3ce37d668b4e4cedd490
Stefan Sabev <stevenbg91@gmail.com>
2024-09-25 12:48:44 +0200

    Add login functionality

--- client/src/dataService/authService.js
+++ client/src/dataService/authService.js
@@ -10,3 +10,14 @@ export const register = async (data) => {
         throw err; 
     }
 };
+
+export const login = async ( values ) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/login`, values, { withCredentials: true });
+        console.log(response);
+        return response.data;
+    } catch (err) {
+        console.error(`Error in login function: ${err}`);
+        throw err;
+    }
+} 
