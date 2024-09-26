dc6aa85e420e53ba8893a803830f9e10237c82d3
Stefan Sabev <stevenbg91@gmail.com>
2024-09-26 12:43:21 +0200

    Refactor some of the functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,14 +13,24 @@ import RegistrationSection from "./components/registrationSection/RegistrationSe
 import ThemeController from "./components/themeController/ThemeController"
 import Login from "./components/auth/login/Login"
 import CoursesPage from "./components/courses/coursesPage/CoursesPage"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import AuthContext from "./context/authContext"
 import Dashboard from "./components/dashboard/DashBoard"
+import SetUserRole from "./utils/setUserRole"
 
 
 function App() {
   const {user} = useContext(AuthContext)
+  SetUserRole();
   console.log(user)
+
+//   useEffect(() => {
+//     if (!user) {
+//         checkAuth()  // Call API to check if JWT in cookie is valid
+//             .then(validUser => setUser(validUser))
+//             .catch(err => console.log("Not authenticated:", err));
+//     }
+// }, [user]);
   
   return (
    
--- client/src/dataService/authService.js
+++ client/src/dataService/authService.js
@@ -1,23 +1,24 @@
 import axios from 'axios';
+import { getErrorMessage } from '../utils/errorParser';
 
 export const register = async (data) => {
-    try {
-        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/register`, data, { withCredentials: true });
-        console.log(response.data);
-        return response.data; 
-    } catch (err) {
-        console.error(`Error in register function: ${err}`);
-        throw err; 
-    }
+try {
+    const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/register`, data, { withCredentials: true });
+    return response.data; 
+} catch (err) {
+    console.error(`Error in register function: ${err}`);
+    const errorMessage = getErrorMessage(err);
+    throw new Error(errorMessage);
+}
 };
 
 export const login = async ( values ) => {
-    try {
-        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/login`, values, { withCredentials: true });
-        console.log(response);
-        return response.data;
-    } catch (err) {
-        console.error(`Error in login function: ${err}`);
-        throw err;
-    }
+try {
+    const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/login`, values, { withCredentials: true });
+    console.log(response);
+    return response.data;
+} catch (err) {
+    console.error(`Error in login function: ${err}`);
+    throw err;
+}
 } 
