daf85a879b550135212265d6f9c26386bee65238
Stefan Sabev <stevenbg91@gmail.com>
2024-09-27 13:00:38 +0200

    Update service and add new function to check auth

--- client/src/dataService/authService.js
+++ client/src/dataService/authService.js
@@ -2,23 +2,33 @@ import axios from 'axios';
 import { getErrorMessage } from '../utils/errorParser';
 
 export const register = async (data) => {
-try {
-    const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/register`, data, { withCredentials: true });
-    return response.data; 
-} catch (err) {
-    console.error(`Error in register function: ${err}`);
-    const errorMessage = getErrorMessage(err);
-    throw new Error(errorMessage);
-}
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/register`, data, { withCredentials: true });
+        return response.data;
+    } catch (err) {
+        console.error(`Error in register function: ${err}`);
+        const errorMessage = getErrorMessage(err);
+        throw new Error(errorMessage);
+    }
 };
 
-export const login = async ( values ) => {
-try {
-    const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/login`, values, { withCredentials: true });
-    console.log(response);
-    return response.data;
-} catch (err) {
-    console.error(`Error in login function: ${err}`);
-    throw err;
+export const login = async (values) => {
+    try {
+        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/auth/login`, values, { withCredentials: true });
+        console.log(response);
+        return response.data;
+    } catch (err) {
+        console.error(`Error in login function: ${err}`);
+        throw err;
+    }
 }
-} 
+
+export const getUserFromCookie = async () => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_EXPRESS_API}/auth/checkAuth`, { withCredentials: true });
+        return response.data;
+    } catch (err) {
+        console.error("Error verifying token:", err);
+        return null;
+    }
+};
