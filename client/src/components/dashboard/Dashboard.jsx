8888506a0022e37a5a0c9ecfbf1b464a4aaa0ce9
Stefan Sabev <stevenbg91@gmail.com>
2024-09-28 12:26:08 +0200

    Update Dashboard.jsx
    Dashboard uses the dynamic navigation in there

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
 import StudentDashboard from "./student/dashboard/Dashboard";
 import StudentNavigation from "./student/navigation/StudentNavigation";
 import TopElement from "./topElement/TopElement";
+import AuthContext from "../../context/authContext";
+import { Route, Routes } from "react-router-dom";
+import MyCourses from "./student/myCourses/MyCourses";
+import Profile from "./student/profile/Profile";
+import Wishlist from "./student/wishlist/Wishlist";
+import Reviews from "./student/reviews/Reviews";
+import Settings from "./student/settings/Settings";
+import roleRoutesConfig from "./roleRoutesConfig";
+
 
 export default function Dashboard() {
-
-    //TODO: to check db for user role and render the appropriate dashboard
+    const { user} = useContext(AuthContext);
+    const currentRole = user?.role || "student";
+    const routes = roleRoutesConfig[currentRole];
+    
     return (
         <>
             <TopElement />
@@ -12,11 +24,15 @@ export default function Dashboard() {
                 <div className="container-fluid-2">
                     <div className="grid grid-cols-1 lg:grid-cols-12 gap-30px pt-30px pb-100px">
                         <div className="lg:col-start-1 lg:col-span-3">
-                            {/* navigation menu */}
-                            <StudentNavigation />
+                            {currentRole === "student" && <StudentNavigation />}
+                            {/* {role === "instructor" && <InstructorNavigation />} */}
                         </div>
                         {/* dashboard content */}
-                        <StudentDashboard />
+                        <Routes>
+                            {routes.map((route, index) => (
+                                <Route key={index} path={route.path} element={route.element} />
+                            ))}
+                        </Routes>
                     </div>
                 </div>
             </section>
