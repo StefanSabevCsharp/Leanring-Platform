389710007fa89521bb1954f9853604d7db0a6e4c
Stefan Sabev <stevenbg91@gmail.com>
2024-09-29 12:24:52 +0200

    Update Password.jsx
    Add functionality to change the password

--- client/src/components/dashboard/student/settings/password/Password.jsx
+++ client/src/components/dashboard/student/settings/password/Password.jsx
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+import useForm  from "../../../../../hooks/useForm";
+import AuthContext from "../../../../../context/authContext";
+import { updateUserPassword } from "../../../../../dataService/authService";
+import { useNavigate } from "react-router-dom";
+
 export default function Password() {
+    const { user , setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const initialValues = {
+        currentPassword: '',
+        newPassword: '',
+        retypePassword: '',
+        _id: user._id
+    }
+
+    const submitHandler = async (values) => {
+        const response = await updateUserPassword(values);
+        setUser(response);
+        navigate('/dashboard');
+    }
+
+    const [ values, onChange, onSubmit ] = useForm(initialValues, submitHandler, "changePassword");
     return (
         <div className="transition-all duration-300">
-            <form
+            <form onSubmit={onSubmit}
                 className="text-sm text-blackColor dark:text-blackColor-dark leading-1.8"
                 data-aos="fade-up"
             >
@@ -11,6 +33,9 @@ export default function Password() {
                             Current Password
                         </label>
                         <input
+                            name="currentPassword"
+                            value={values.currentPassword}
+                            onChange={onChange}
                             type="password"
                             placeholder="Current password"
                             className="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no"
@@ -19,6 +44,9 @@ export default function Password() {
                     <div>
                         <label className="mb-3 block font-semibold">New Password</label>
                         <input
+                            name="newPassword"
+                            value={values.newPassword}
+                            onChange={onChange}
                             type="password"
                             placeholder="New Password"
                             className="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no"
@@ -29,6 +57,9 @@ export default function Password() {
                             Re-Type New Password
                         </label>
                         <input
+                            name="retypePassword"
+                            value={values.retypePassword}
+                            onChange={onChange}
                             type="password"
                             placeholder="Re-Type New Password"
                             className="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no"
