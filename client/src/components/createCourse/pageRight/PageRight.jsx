414b93eebfccecdd40853e255b333fc64bbef3be
Stefan Sabev <stevenbg91@gmail.com>
2024-09-30 12:20:42 +0200

    Add two more components for the create course page

---
+++ client/src/components/createCourse/pageRight/PageRight.jsx
@@ -0,0 +1,143 @@
+export default function PageRight() {
+  return (
+    <div data-aos="fade-up" className="lg:col-start-9 lg:col-span-4">
+    <div className="p-30px border-2 border-primaryColor">
+        <ul>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Set the Course Price option make it free.
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Standard size for the course thumbnail.
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Video section controls the course overview video.
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Course Builder is where you create course.
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Add Topics in the Course Builder section to create lessons, .
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Prerequisites refers to the fundamental courses .
+                </p>
+            </li>
+            <li className="my-7px flex gap-10px">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={14}
+                    height={14}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-check flex-shrink-0"
+                >
+                    <polyline points="20 6 9 17 4 12" className="text-greencolor" />
+                </svg>
+                <p className="text-lg text-contentColor dark:text-contentColor-dark leading-1.45">
+                    Information from the Additional Data section.
+                </p>
+            </li>
+        </ul>
+    </div>
+</div>
+  );
+}
