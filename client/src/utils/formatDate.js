e3cb61e43a8e3bbfa35a2d5532638b6fcca2d6e0
Stefan Sabev <stevenbg91@gmail.com>
2024-10-10 18:46:57 +0200

    Util function to show the date properly

---
+++ client/src/utils/formatDate.js
@@ -0,0 +1,10 @@
+
+
+export function formatDate(dateString) {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+    }).format(date);
+}
