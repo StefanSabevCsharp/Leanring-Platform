0c0df383bc8bfe5746f4fc8ba55739e6857bdc53
Stefan Sabev <stevenbg91@gmail.com>
2024-10-12 17:12:29 +0200

    Create comment schema

---
+++ server/src/schemas/commentSchema.js
@@ -0,0 +1,24 @@
+const { Schema } = require("mongoose");
+
+const Shema = require("mongoose").Schema;
+const model = require("mongoose").model;
+
+const commentSchema = new Schema({
+    text:{
+        type: String,
+        required: true
+    },
+    course:{
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    creator:{
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = model("Comment", commentSchema);
