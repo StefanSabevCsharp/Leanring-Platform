b42195bfc3d48d497641f52237b777c9319c80fb
Stefan Sabev <stevenbg91@gmail.com>
2024-10-12 17:12:55 +0200

    Add new route for reviews controller

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const authController = require("./controllers/authController");
 const courseController = require("./controllers/courseController");
 const commentController = require("./controllers/commentController");
+const reviewController = require("./controllers/reviewController");
 
-router.use("/auth",authController);
-router.use("/courses",courseController);
-router.use("/comments",commentController);
+router.use("/auth", authController);
+router.use("/courses", courseController);
+router.use("/comments", commentController);
+router.use("/reviews", reviewController);
 
 module.exports = router;
