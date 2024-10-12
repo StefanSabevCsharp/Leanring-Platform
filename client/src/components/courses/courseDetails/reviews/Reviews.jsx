b8e3811649ebf4b0e5a9253d0c627c47bf632dd2
Stefan Sabev <stevenbg91@gmail.com>
2024-10-12 17:17:31 +0200

    Create Review component
    Dynamically shows progress bar and calculates every reviews start to populate real data. Render the review form depending if user is logged in .

---
+++ client/src/components/courses/courseDetails/reviews/Reviews.jsx
@@ -0,0 +1,76 @@
+import { useContext } from "react";
+import ReviewForm from "./reviewForm/ReviewForm";
+import SingleReview from "./singleReview/SingleReview";
+import AuthContext from "../../../../context/authContext";
+
+export default function Reviews({reviews, setReviews, courseId, user}) {
+  console.log(reviews);
+  const calculateAvarageRating = () => {
+    const totalRating = reviews.reduce((acc, review) => acc + review.stars, 0);
+    return totalRating / reviews.length;
+  }  
+  const getReviewsCountByStars = (star) => {
+    return reviews.filter((review) => review.stars === star).length;
+  };
+  const getStarPercentage = (star) => {
+    const starCount = getReviewsCountByStars(star);
+    return (starCount / reviews.length) * 100; 
+  };
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-12 items-center gap-x-30px gap-y-5">
+        <div className="lg:col-start-1 lg:col-span-4 px-10px py-30px bg-whiteColor dark:bg-whiteColor-dark shadow-review text-center">
+          <p className="text-7xl font-extrabold text-blackColor dark:text-blackColor-dark leading-90px">
+            {calculateAvarageRating().toFixed(1)}
+          </p>
+          <div className="text-secondaryColor">
+            <i className="icofont-star" />
+            <i className="icofont-star" />
+            <i className="icofont-star" />
+            <i className="icofont-star" />
+            <i className="icofont-star" />
+          </div>
+          <p className="text-blackColor dark:text-blackColor-dark leading-26px font-medium">
+            ({reviews.length} Reviews)
+          </p>
+        </div>
+        {/* progress bar */}
+        <div className="lg:col-start-5 lg:col-span-8 px-15px">
+          <ul className="flex flex-col gap-y-3">
+          {[5, 4, 3, 2, 1].map((star) => (
+          <li key={star} className="flex items-center text-blackColor dark:text-blackColor-dark">
+            <div>
+              <span>{star}</span>
+              <span>
+                <i className="icofont-star text-secondaryColor" />
+              </span>
+            </div>
+            <div className="flex-grow relative mx-10px md:mr-10 lg:mr-10px">
+              <span className="h-10px w-full bg-borderColor dark:bg-borderColor-dark rounded-full block" />
+              <span
+                className="absolute left-0 top-0 h-10px bg-secondaryColor rounded-full"
+                style={{ width: `${getStarPercentage(star)}%` }} 
+              />
+            </div>
+            <div>
+              <span>{getReviewsCountByStars(star)}</span>
+            </div>
+          </li>
+        ))}
+          </ul>
+        </div>
+      </div>
+      {/* client reviews */}
+      <div className="mt-60px mb-10">
+        <h4 className="text-lg text-blackColor dark:text-blackColor-dark font-bold pl-2 before:w-0.5 relative before:h-[21px] before:bg-secondaryColor before:absolute before:bottom-[5px] before:left-0 leading-1.2 mb-25px">
+          Customer Reviews
+        </h4>
+        <ul>
+          {reviews.map((review) => (<SingleReview key={review._id} review={review} />))}
+        </ul>
+      </div>
+      {/* add reviews */}
+     {user && <ReviewForm setReviews={setReviews} courseId={courseId} user={user} />}
+    </>
+  );
+}
