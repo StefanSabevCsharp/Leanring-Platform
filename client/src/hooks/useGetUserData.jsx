d5c181d594dcdf8327388c44c4ff5c8aaf8ad755
Stefan Sabev <stevenbg91@gmail.com>
2024-10-12 09:28:04 +0200

    Create new hook
    Added new hook to simple retrieve of userdata

---
+++ client/src/hooks/useGetUserData.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { getErrorMessage } from "../utils/errorParser";
+import { getFullUserData } from "../dataService/authService";
+
+
+export default function useGetUserData(userId) {
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    //TODO : fix error handling
+
+    useEffect(() => {
+        setLoading(true);
+        try{
+            const getData = async () => {
+                const response = await getFullUserData(userId);
+                setUserData(response);
+                setLoading(false);
+            }
+            getData();
+        
+        }catch(error){
+            console.log("Error in useGetUserData hook", error);
+            return getErrorMessage(error);
+        }
+        
+    }, [userId]);
+
+    return [userData, loading];
+}
