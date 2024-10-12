24e9a972da1761dfb333d61eac85510ad0b27bbd
Stefan Sabev <stevenbg91@gmail.com>
2024-10-12 17:13:33 +0200

    Create review controller
    Receive the data and create new review linked with user and course

---
+++ server/src/controllers/reviewController.js
@@ -0,0 +1,49 @@
+const authenticateToken = require("../middlewares/authMiddleware");
+const router = require("express").Router();
+const Course = require("../schemas/courseSchema");
+const User = require("../schemas/authSchema");
+const Review = require("../schemas/reviewSchema");
+const { getErrorMessage } = require("../utils/errorParser");
+
+
+router.post("/create", authenticateToken, async (req, res) => {
+    console.log(req.body);
+    const { reviewData, courseId, userId } = req.body;
+    if(!reviewData || !courseId || !userId){
+        return res.status(400).json({ message: "Review data, course ID and user ID are required." });
+    }
+    try{
+        const review = new Review({
+            ...reviewData,
+            course: courseId,
+            user: userId,
+            creator: req.user.firstName + " " + req.user.lastName
+        })
+        const savedReview = await review.save();
+        if(!savedReview){
+            return res.status(400).json({ message: "Error saving the review" });
+        } 
+        const course = Course.findById(courseId);
+        const user = User.findById(userId);
+        const [foundCourse, foundUser] = await Promise.all([course, user]);
+        if(!foundCourse){
+            return res.status(400).json({ message: "Course not found" });
+        }
+        if(!foundUser){
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        foundUser.reviews.push(savedReview._id);
+        foundCourse.reviews.push(savedReview._id);
+        await Promise.all([foundUser.save(), foundCourse.save()]);
+        return res.status(201).json({ message: "Review created successfully", review: savedReview });
+ 
+    }catch(error){
+        console.error("Error in create review function", error);
+        const errorMessage = getErrorMessage(error);
+        throw new Error(errorMessage);
+    }
+});
+
+
+module.exports = router;
