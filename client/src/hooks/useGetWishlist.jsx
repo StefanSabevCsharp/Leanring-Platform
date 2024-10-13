a4d95514d30bd7d07aa1dee0f9f9dde6c46eb3c7
Stefan Sabev <stevenbg91@gmail.com>
2024-10-13 10:17:08 +0200

    Create custom hook to get courses in Wishlist

---
+++ client/src/hooks/useGetWishlist.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { getWishListCourses } from "../dataService/wishlistService";
+
+export default function useGetWishlist(userId) {
+    const [wishlist, setWishlist] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchWishlist = async () => {
+            try {
+                const response = await getWishListCourses(userId);
+                console.log(response);
+                setWishlist(response);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchWishlist();
+    }, [userId]);
+
+    return [wishlist, isLoading, error];
+}
