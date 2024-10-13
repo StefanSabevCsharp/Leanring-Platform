26bb54eb90d87eaa57354d0307766ee696ecb271
Stefan Sabev <stevenbg91@gmail.com>
2024-10-13 10:11:00 +0200

    Update Comments.jsx
    Use Pagination for the comment section in Course details page

--- client/src/components/courses/courseDetails/comments/Comments.jsx
+++ client/src/components/courses/courseDetails/comments/Comments.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import CourseComment from "../../../courseComment/CourseComment";
+import Pagination from "../../../pagination/Pagination";
 
 export default function Comments({ comments }) {
+    const [page, setPage] = useState(1);
+
+    const commentsPerPage = 5;
+    const indexOfLastComment = page * commentsPerPage;
+    const indexOfFirstComment = indexOfLastComment - commentsPerPage;
+    const currentComments = comments.slice(indexOfFirstComment, indexOfLastComment);
+    const totalPages = Math.ceil(comments.length / commentsPerPage);
+    
     //TODO : add pagination
     return (
+        <>
         <div className="pt-50px pb-15px border-y border-borderColor2 dark:border-borderColor2-dark">
             <h4
                 className="text-size-26 font-bold text-blackColor dark:text-blackColor-dark mb-30px !leading-30px"
@@ -13,8 +24,10 @@ export default function Comments({ comments }) {
             </h4>
             <ul>
 
-                {comments.map((comment) => (<CourseComment comment={comment} key={comment._id} />))}
+                {currentComments.map((comment) => (<CourseComment comment={comment} key={comment._id} />))}
             </ul>
         </div>
+        <Pagination  setPage={setPage} totalPages={totalPages} page={page}/>
+        </>
     );
 }
