4e8514609dcafef509550c77b88c7fe79770b532
Stefan Sabev <stevenbg91@gmail.com>
2024-10-13 10:19:53 +0200

    Function to remove course from Wishlist

--- client/src/dataService/wishlistService.js
+++ client/src/dataService/wishlistService.js
@@ -14,3 +14,16 @@ export const addCourseToWishList = async (courseId, userId) => {
     }
 };
 
+export const removeCourseFromWishlist = async (courseId, userId) => {
+
+    try{
+        const response = await axios.post(`${import.meta.env.VITE_EXPRESS_API}/wishlist/remove`, {courseId, userId }, { withCredentials: true });
+        return response.data;
+
+    }catch(error){
+        const errorMessage = getErrorMessage(error);
+        console.log(errorMessage);
+        return new Error(errorMessage);
+    }
+}
+
