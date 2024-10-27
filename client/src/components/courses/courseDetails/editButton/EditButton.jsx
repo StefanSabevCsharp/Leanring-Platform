96157d869c0f33ef029ca03a872c855a1a536def
Stefan Sabev <stevenbg91@gmail.com>
2024-10-27 13:27:56 +0100

    Separated logic for editing course

---
+++ client/src/components/courses/courseDetails/editButton/EditButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ConfirmModal from "../../../modal/Modal";
+
+export default function EditButton({ course }) {
+    console.log(course);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const navigate = useNavigate();
+
+    const handleEdit = async () => {
+        
+        navigate(`/courses/edit/${course._id}`);
+    };
+    return (
+        <>
+            <button
+                onClick={() => setShowConfirmModal(true)}
+                type="button"
+                className="w-full text-size-15 text-whiteColor bg-primaryColor px-25px py-10px border mb-10px leading-1.8 border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark"
+            >
+                Edit
+            </button>
+            {showConfirmModal && (
+                <ConfirmModal
+                    message="Are you sure you want to EDIT this course?"
+                    onConfirm={handleEdit}
+                    onCancel={() => setShowConfirmModal(false)}
+                />
+            )}
+        </>
+    );
+}
