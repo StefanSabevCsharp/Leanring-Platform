8de227fa992350b87e37c316ae0a123ea079c76d
Stefan Sabev <stevenbg91@gmail.com>
2024-11-02 17:12:54 +0100

    Create InProgress.jsx

---
+++ client/src/components/inProgress/InProgress.jsx
@@ -0,0 +1,10 @@
+export default function InProgress() {
+    return (
+        <div className="mb-6 pb-5 border-b-2 border-borderColor dark:border-borderColor-dark">
+        <h2 className="text-2xl font-bold text-blackColor dark:text-blackColor-dark whitespace-nowrap">
+            In Progress
+        </h2>
+    </div>
+    
+    );
+}
