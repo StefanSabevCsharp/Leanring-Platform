b155c7a84ed20d018f7fb113be16fca6d4f49c3b
Stefan Sabev <stevenbg91@gmail.com>
2024-11-02 17:16:25 +0100

    Create CreateBlog.jsx

---
+++ client/src/components/blog/createBlog/CreateBlog.jsx
@@ -0,0 +1,12 @@
+import AbstractBanner from "../../abstractBanner/AbstractBanner";
+import FourthForm from "../../createCourse/fourthForm/FourthForm";
+
+export default function CreateBlog() {
+  
+  return (
+    <>
+      <AbstractBanner name="Create Blog" path="Home" page="Create Blog" />
+      <FourthForm />
+    </>
+  );
+}
