c2ffa12468101fd00e9821c7f76891ae1dac75e8
Stefan Sabev <stevenbg91@gmail.com>
2024-11-03 08:17:58 +0100

    Add new blog route

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,11 +4,13 @@ const courseController = require("./controllers/courseController");
 const commentController = require("./controllers/commentController");
 const reviewController = require("./controllers/reviewController");
 const wishlistController = require("./controllers/wishlistController");
+const blogController = require("./controllers/blogController");
 
 router.use("/auth", authController);
 router.use("/courses", courseController);
 router.use("/comments", commentController);
 router.use("/reviews", reviewController);
 router.use("/wishlist", wishlistController);
+router.use("/blog", blogController);
 
 module.exports = router;
