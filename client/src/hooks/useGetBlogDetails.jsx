1b9a1d80e02cd5377ff779414cc879ca40d2897c
Stefan Sabev <stevenbg91@gmail.com>
2024-11-03 10:05:10 +0100

    Create custom hook for getting blog data

---
+++ client/src/hooks/useGetBlogDetails.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getBlogDetails } from "../dataService/blogService";
+
+export default function useGetBlogDetails(blogId) {
+    const [blog, setBlog] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        try {
+            const getBlog = async () => {
+                const response = await getBlogDetails(blogId);
+                console.log(response);
+                setBlog(response);
+                setLoading(false);
+            };
+            getBlog();
+
+        } catch (error) {
+            setError(error);
+            setLoading(false);
+        }
+    }, [blogId]);
+
+    return [blog, loading, error];
+
+}
