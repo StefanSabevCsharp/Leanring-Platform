d00d1a9b39fccf3af6e4f52f091b4051ed082d70
Stefan Sabev <stevenbg91@gmail.com>
2024-11-04 19:07:40 +0100

    Update SmallBlogCard.jsx

--- client/src/components/news/smallBlog/SmallBlogCard.jsx
+++ client/src/components/news/smallBlog/SmallBlogCard.jsx
@@ -1,25 +1,26 @@
-export default function SmallBlogCard() {
+import { Link } from "react-router-dom";
+import { formatDate } from "../../../utils/formatDate";
+
+export default function SmallBlogCard({ blog }) {
     return (
         <div className="group shadow-blog" data-aos="fade-up">
             {/* blog thumbnail */}
             <div className="overflow-hidden relative">
                 <img
-                    src="assets/images/blog/blog_3.png"
+                    src={blog.mainImageUrl}
                     alt=""
                     className="w-full group-hover:scale-110 transition-all duration-300"
                 />
                 <div className="text-base md:text-2xl leading-5 md:leading-30px font-semibold text-white px-15px py-5px md:px-22px md:py-7px bg-primaryColor rounded text-center absolute top-5 left-5">
-                    20 <br />
-                    Oct
+                    {formatDate(blog.createdAt)}
                 </div>
             </div>
             {/* blog content */}
             <div className="px-5 py-25px">
                 <h3 className="text-2xl md:text-size-28 leading-30px md:leading-35px font-bold text-blackColor hover:text-primaryColor dark:text-blackColor-dark dark:hover:text-primaryColor">
-                    <a href="blog-details.html">
-                        It is a long established fact that a reader will be Standard
-                        Part
-                    </a>
+                    <Link onClick={() => window.scrollTo({top:300, behavior:"smooth"})} to={`/blog/${blog._id}`}>
+                       {blog.title}
+                    </Link>
                 </h3>
             </div>
         </div>
