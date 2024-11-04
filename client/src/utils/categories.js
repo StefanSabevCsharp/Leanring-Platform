d4847010424858002747d034fa6369d82cc6bff5
Stefan Sabev <stevenbg91@gmail.com>
2024-11-04 19:04:40 +0100

    Create categories.js
    Abstract routes to render in blog and courses

---
+++ client/src/utils/categories.js
@@ -0,0 +1,11 @@
+export const categories = [
+    { title: "All", path: "/courses" },
+    { title: "Web Design", path: "/courses?category=Web Design"},
+    { title: "Graphic", path: "/courses?category=Graphic" },
+    { title: "English", path: "/courses?category=English" },
+    { title: "Spoken English", path: "/courses?category=Spoken English"},
+    { title: "Art Painting", path: "/courses?category=Art Painting"},
+    { title: "App Development", path: "/courses?category=App Development"},
+    { title: "Web Application", path: "/courses?category=Web Application" },
+    { title: "PHP Development", path: "/courses?category=PHP Development" },
+];
