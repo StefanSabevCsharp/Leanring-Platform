16ede34e8d271a3452e323172090a0eab1023814
Stefan Sabev <stevenbg91@gmail.com>
2024-11-06 17:54:32 +0100

    Create ScrollToTop.jsx

---
+++ client/src/components/scrollToTop/ScrollToTop.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname.startsWith("/courses/") || location.pathname.startsWith("/blog/")) {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    }, [location]);
+
+    return null; 
+}
