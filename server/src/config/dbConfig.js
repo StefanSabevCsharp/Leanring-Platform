15a2ac788418c69bd5bc550071df4d02e58a8859
Stefan Sabev <stevenbg91@gmail.com>
2024-11-13 19:27:09 +0100

    Update dbConfig.js

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 function dBConfig(){
 
-    mongoose.connect(process.env.MONGO_URI).then(() => {
+    // mongoose.connect(process.env.MONGO_URI).then(() => {
+    //     console.log("Database connected successfully");
+    // }).catch((err) => {
+    //     console.log("Error connecting to database", err);
+    // });
+    mongoose.connect(process.env.MONGO_URI, {
+        serverSelectionTimeoutMS: 30000, 
+        socketTimeoutMS: 30000,    
+      }).then(() => {
         console.log("Database connected successfully");
     }).catch((err) => {
         console.log("Error connecting to database", err);
-    });
+    });;
 
 }
 
